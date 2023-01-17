63c803a6a73dcde1310006ce39c11ac95960f372
EricCQuintal <7302@itescam.edu.mx>
2023-01-17 10:48:22 -0600

    otra

--- EricQuintal/prueba.java
+++ EricQuintal/prueba.java
@@ -1,5 +1,13 @@
-public static void main(String[] args) {
-    System.out.println ("hola que hace ");
+package EricQuintal;
 
+/**
+ * prueba
+ */
+public class prueba {
+
+    public static void main(String[] args) {
+        System.out.println ("hola que hace como esta " );  
+    
+    }
 }
 
